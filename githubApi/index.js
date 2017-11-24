9975cbd7f76294c2c72a93d5a2f7416e7078bc2a
Dima Smirnov <email@dimasmirnov.ru>
2017-11-24 21:26:00 +0300

    created git api

--- githubApi/index.js
+++ githubApi/index.js
@@ -1,18 +1,20 @@
-var request = require('request');
+const axios = require('axios');
+const moment = require('moment');
 
-var options = {
-  url: 'https://api.github.com/repos/smff/haklelon2017/merges',
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Authorization': 'token 19b339e826c6268e03365c0d4e7b8fd3cf50d9e3',
-    'User-Agent': 'Awesome-Octocat-App',
+axios.post(
+  'https://api.github.com/repos/smff/haklelon2017/merges',
+  {
+    base: 'master',
+    head: 'dev',
+    commit_message: 'hello from ninel ' + moment().toISOString(),
+  },
+  {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': 'token abd061e1067ad3876091424fd17cecffd4c3f90c',
+      'User-Agent': 'Awesome-Octocat-App',
+    },
   }
-};
-
-request(options, callback);
-
-function callback(error, response, body) {
-  console.log(response);
-  console.log(body);
-}
+)
+  .then(() => console.log('ok'))
+  .catch(() => console.log('error'));
